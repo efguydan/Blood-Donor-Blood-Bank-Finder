77fc387d34edab4c7c81f3d7934102ae9bea5827
EfeDaniel <EfeEjemudaro@gmail.com>
2019-12-17 00:09:26 +0100

    chore: clean up code

--- app/src/main/java/com/efedaniel/bloodfinder/bloodfinder/maps/selectlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/efedaniel/bloodfinder/bloodfinder/maps/selectlocation/SelectLocationFragment.kt
@@ -57,10 +57,10 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         //TODO move map to users current location by default
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Nigeria and move the camera
+        val nigeria = LatLng(9.08, 8.67)
+//        map.addMarker(MarkerOptions().position(nigeria).title("Marker in Nigeria"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(nigeria))
     }
 
     private fun setUpToolbar() = mainActivity.run {
